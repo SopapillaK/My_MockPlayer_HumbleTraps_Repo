a488568db40ded224aed5df095984419646c018f
SopapillaKat <vellita2011@gmail.com>
2023-12-02 15:42:59 -0700

    dev in

---
+++ MockPlayers_HumbleTraps/Assets/Test/Editor/TrapTests.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using NSubstitute;
+
+public class TrapTests 
+{
+    [Test]
+    public void PlayerEntering_PlayerTargetedTrap_ReducesHealthByOne()
+    {
+        //Arrange
+        Trap trap = new Trap();
+        ICharacterMover characterMover = Substitute.For<ICharacterMover>(); 
+        characterMover.IsPlayer.Returns(true);
+
+        //Act
+        trap.HandleCharacterEntered(characterMover, TrapTargetType.Player);
+
+        //Assert
+        Assert.AreEqual(-1, characterMover.Health);
+    }
+
+    [Test]
+    public void NpcEntering_NpcTargetedTrap_ReducesHealthByOne()
+    {
+        Trap trap = new Trap();
+        ICharacterMover characterMover = Substitute.For<ICharacterMover>();
+        trap.HandleCharacterEntered(characterMover, TrapTargetType.Npc);
+        Assert.AreEqual(-1, characterMover.Health);
+    }
+}
